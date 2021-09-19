9963b14c739a31e750a840d46df3310c9279cdb4
Thales Silva <tferreira@gavea.com>
2021-09-19 10:54:59 -0300

    refactor: add normalize css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./pages/home";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Home />
     </div>
   );
