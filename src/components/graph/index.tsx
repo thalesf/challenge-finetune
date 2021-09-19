9417c7877cb2a3e3ce437e280c5ddb89e316b2c8
Thales Silva <tferreira@gavea.com>
2021-09-19 13:06:20 -0300

    refactor: add filterByType function to graph

--- src/components/graph/index.tsx
+++ src/components/graph/index.tsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
+import { useList } from "../../hooks/useList";
+import { ItemType } from "../../types/list";
+import { filterByType } from "../../utils/count";
 
-const data = {
-  labels: ["Protein", "Fat", "Carbohydrates"],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [300, 50, 100],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
-      ],
-      hoverOffset: 4,
-    },
-  ],
-};
 export default function Chart() {
+  const { listItem } = useList();
+
+  console.log("LIST ITEM", listItem);
   return (
     <div>
       <Pie
         style={{ width: 200, height: 200 }}
-        data={data}
+        data={{
+          labels: ["Protein", "Fat", "Carbohydrates"],
+
+          datasets: [
+            {
+              label: "My First Dataset",
+              data: [
+                filterByType(listItem, ItemType.Protein),
+                filterByType(listItem, ItemType.Fat),
+                filterByType(listItem, ItemType.Carbohydrate),
+              ],
+              backgroundColor: [
+                "rgb(255, 99, 132)",
+                "rgb(54, 162, 235)",
+                "rgb(255, 205, 86)",
+              ],
+              hoverOffset: 4,
+            },
+          ],
+        }}
         options={{ maintainAspectRatio: false }}
       />
     </div>
