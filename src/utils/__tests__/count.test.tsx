b894605553080f97b406a00a6883975ed5b0fcf9
Thales Silva <tferreira@gavea.com>
2021-09-19 12:57:23 -0300

    test: should return zero when invalid id is provided

--- src/utils/__tests__/count.test.tsx
+++ src/utils/__tests__/count.test.tsx
@@ -1,35 +1,34 @@
 import { filterByType } from "../count";
 
+const mockItemList = [
+  {
+    date: "2021-09-18T22:58:23.383Z",
+    name: "Rice",
+    amount: 200,
+    type: 1,
+    id: 1,
+  },
+  {
+    date: "2021-09-18T22:58:23.383Z",
+    name: "Beans",
+    amount: 100,
+    type: 2,
+    id: 2,
+  },
+  {
+    date: "2021-09-18T22:58:23.383Z",
+    name: "Beans",
+    amount: 100,
+    type: 2,
+    id: 3,
+  },
+];
 test("should return quantity of items by type", () => {
-  const mockItemList = [
-    {
-      date: "2021-09-18T22:58:23.383Z",
-      name: "Rice",
-      amount: 200,
-      type: 1,
-      id: 1,
-    },
-    {
-      date: "2021-09-18T22:58:23.383Z",
-      name: "Beans",
-      amount: 100,
-      type: 2,
-      id: 2,
-    },
-    {
-      date: "2021-09-18T22:58:23.383Z",
-      name: "Beans",
-      amount: 100,
-      type: 2,
-      id: 3,
-    },
-  ];
   const filtered = filterByType(mockItemList, 1);
-  expect(filtered).toBe(1)
+  expect(filtered).toBe(1);
 });
 
-test("should return an empty array when list item is not valid", () => {
-  const mockItemList = [{ id: 2, another_data: 123 }];
-  const filtered = filterByType(mockItemList, 1);
-  expect(filtered).toEqual([]);
+test("should return zero when invalid id is provided", () => {
+  const filtered = filterByType(mockItemList, 9999);
+  expect(filtered).toBe(0);
 });
