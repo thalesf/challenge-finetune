35792ed192b8b5516306ab4af70845aeca4d7486
Thales Silva <tferreira@gavea.com>
2021-09-19 13:58:01 -0300

    test: should return a list of items by date

---
+++ src/utils/__tests__/filter.test.tsx
@@ -0,0 +1,25 @@
+import { filterByDate } from "../filter";
+import { isEqual } from "date-fns";
+test("should return a list of elements by date", () => {
+  const mockItemList = [
+    {
+      date: "2021-09-18T00:00:00.000Z",
+      name: "Rice",
+      amount: 200,
+      type: 1,
+      id: 1,
+    },
+    {
+      date: "2021-09-10T00:00:00.000Z",
+      name: "Beans",
+      amount: 100,
+      type: 2,
+      id: 2,
+    },
+  ];
+
+  const date = "2021-09-18T00:00:00.000Z";
+
+  const result = filterByDate(mockItemList, date);
+  expect(result).toBe(date);
+});
