9047454f766b643c6b3ca159b09567473a0e62b8
Thales Silva <tferreira@gavea.com>
2021-09-19 11:54:29 -0300

    feat: add new item to list and formik validation

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -4,7 +4,7 @@ import {
   Modal,
   Grid,
   Typography,
-  TextField,
+  // TextField,
   InputLabel,
   Button,
   FormControl,
@@ -12,64 +12,111 @@ import {
   Select,
   FormHelperText,
 } from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import { Close as CloseIcon } from "@mui/icons-material";
+import { useList } from "../../hooks/useList";
+import TextField from "@material-ui/core/TextField";
+import { ItemType } from "../../types/list";
+
+const validationSchema = yup.object({
+  name: yup.string().required("Name is required"),
+  amount: yup
+    .number()
+    .min(1, "Amount should be higher than 0")
+    .required("Amount is required"),
+  type: yup.number().required("Type is required"),
+});
 
 export default function FormAddItem() {
   const [age, setAge] = React.useState("");
+  const { addNewItem } = useList();
 
-  const handleChange = (event: any) => {
-    setAge(event.target.value);
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      amount: "",
+      type: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      addNewItem({
+        name: values.name,
+        amount: parseInt(values.amount),
+        type: parseInt(values.type)
+      });
+    },
+  });
+
+  const handleChangeType = (e: any) => {
+    // setAge(event.target.value);
+    console.log('E',e)
+    formik.setFieldValue("type", e.target.value);
   };
 
   return (
-    <Grid
-      sx={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: 400,
-        bgcolor: "background.paper",
-        boxShadow: 24,
-        p: 4,
-      }}
-      md={6}
-    >
-      <CloseIcon />
+    <form onSubmit={formik.handleSubmit}>
+      {console.log("FORMIK", formik.values)}
+      <Grid
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        <CloseIcon />
 
-      <Typography>Olá mundo</Typography>
-      <TextField
-        id="outlined-basic"
-        label="Outlined"
-        variant="outlined"
-        placeholder="Name"
-      />
-      <TextField
-        id="outlined-basic"
-        label="Outlined"
-        variant="outlined"
-        placeholder="Amount"
-      />
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={age}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>With label + helper text</FormHelperText>
-      </FormControl>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained">Cadastrar</Button>
-    </Grid>
+        <Typography>Register Item</Typography>
+        <FormControl sx={{ m: 1, minWidth: 200 }}>
+          <TextField
+            id="name"
+            label="Name"
+            variant="outlined"
+            name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            error={formik.touched.name && Boolean(formik.errors.name)}
+            helperText={formik.touched.name && formik.errors.name}
+          />
+        </FormControl>
+
+        <FormControl sx={{ m: 2, minWidth: 200 }}>
+          <TextField
+            id="amount"
+            label="Amount"
+            variant="outlined"
+            placeholder="Amount"
+            name="amount"
+            value={formik.values.amount}
+            onChange={formik.handleChange}
+            error={formik.touched.amount && Boolean(formik.errors.amount)}
+            helperText={formik.touched.amount && formik.errors.amount}
+          />
+        </FormControl>
+
+        <FormControl sx={{ m: 2, minWidth: 200 }}>
+          <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
+          <Select
+            id="type"
+            value={age}
+            label="Type"
+            onChange={handleChangeType}
+          >
+            <MenuItem value={ItemType.Protein}>Protein</MenuItem>
+            <MenuItem value={ItemType.Fat}>Fat</MenuItem>
+            <MenuItem value={ItemType.Carbohydrate}>Carbohydrate</MenuItem>
+          </Select>
+          <FormHelperText>Item Type</FormHelperText>
+        </FormControl>
+        <Button variant="contained" type="submit">
+          Cadastrar
+        </Button>
+      </Grid>
+    </form>
   );
 }
--- src/types/list.tsx
+++ src/types/list.tsx
@@ -1,7 +1,20 @@
 export interface ListItem {
+  // date: string;
+  name: string;
+  amount: number;
+  type: number;
+}
+
+export interface ListItemResponse {
   date: string;
   name: string;
   amount: number;
   type: number;
   id: number;
+}
+
+export enum ItemType {
+  Protein = 1,
+  Fat = 2,
+  Carbohydrate = 3 
 }
