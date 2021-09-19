e0719a17bb89507af40c0450a3a3749597961917
Thales Silva <tferreira@gavea.com>
2021-09-19 09:43:17 -0300

    refactor: add api mock

---
+++ src/mocks/listItem.ts
@@ -0,0 +1,16 @@
+export const mockItemList = [
+    {
+      date: "2021-09-18T22:58:23.383Z",
+      name: "Rice",
+      amount: 200,
+      type: 1,
+      id: 1,
+    },
+    {
+      date: "2021-09-18T22:58:23.383Z",
+      name: "Beans",
+      amount: 100,
+      type: 1,
+      id: 2,
+    },
+  ];
