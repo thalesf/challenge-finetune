46133f7a8e6fe5fd5027e127be5ba10561d1377c
Thales Silva <tferreira@gavea.com>
2021-09-19 14:49:59 -0300

    refactor: return items by date

--- src/utils/filter.tsx
+++ src/utils/filter.tsx
@@ -1,14 +1,23 @@
 import { ListItem } from "../types/list";
-import { isValid } from "date-fns";
+import { getYear, getMonth, getDate, isEqual } from "date-fns";
 
 export function filterByDate(list: ListItem[], date: string) {
-  return [
-    {
-      amount: 200,
-      date: "2021-09-18T00:00:00.000Z",
-      id: 1,
-      name: "Rice",
-      type: 1,
-    },
-  ];
+  const filteredData = new Date(date);
+  // const
+  return list.filter((item: ListItem) => {
+    console.log("NEW DATE", item.date, filteredData);
+    console.log("FILTEREED", getYear(new Date(item.date)));
+    return (
+      new Date(
+        getYear(new Date(item.date)),
+        getMonth(new Date(item.date)),
+        getDate(new Date(item.date))
+      ),
+      new Date(
+        getYear(new Date(filteredData)),
+        getMonth(new Date(filteredData)),
+        getDate(new Date(filteredData))
+      )
+    );
+  });
 }
