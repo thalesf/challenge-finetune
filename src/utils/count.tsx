4d6f950cc370aa8f6d7d6ad63ff18a141d443247
Thales Silva <tferreira@gavea.com>
2021-09-19 12:57:51 -0300

    refactor: filterByType function changing parameter name

--- src/utils/count.tsx
+++ src/utils/count.tsx
@@ -3,5 +3,5 @@ import { ListItem } from "../types/list";
 export function filterByType(list: ListItem[], id: number) {
   return list
     .reduce((accumulator, currentValue) => accumulator.concat(currentValue), [] as ListItem[])
-    .filter((item:ListItem) => item.type === 1);
+    .filter((item:ListItem) => item.type === id).length;
 }
